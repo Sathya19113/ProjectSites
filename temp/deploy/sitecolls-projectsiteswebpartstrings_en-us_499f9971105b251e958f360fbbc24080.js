fc24b36e38fe14e16e99b44e2a88791fd3cc9acb
Sathya19113 <34091882+Sathya19113@users.noreply.github.com>
2018-05-02 10:29:24 +0530

    Add files via upload

---
+++ temp/deploy/sitecolls-projectsiteswebpartstrings_en-us_499f9971105b251e958f360fbbc24080.js
@@ -0,0 +1,9 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Project Sites configuration",
+    "BasicGroupName": "Group Name",
+    "DescriptionFieldLabel": "Description Field",
+    "RegionFieldLabel": "Select Region List Name",
+    "ListFieldLabel": ""
+  }
+});
